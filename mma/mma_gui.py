28ddce149d521cfca35b50fc868846b6adee46fc
anamaria-uofg <>
2021-07-19 12:57:02 +0100

    modified int key from file uploader to unicode

--- mma/mma_gui.py
+++ mma/mma_gui.py
@@ -41,7 +41,7 @@ def main():
 
     for i in range(datasets_number_option):
         datasets[i] = st.sidebar.file_uploader('Upload files containing SRM information for dataset '+str(i+1),
-                                                type = ['csv'], key = chr(i),
+                                                type = ['csv'], key = chr(i+1),
                                                 accept_multiple_files = True )
 
         srms[i] = []
