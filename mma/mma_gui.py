7fe9265b8f17b9c5129d5480bce5e5de5103c07d
anamaria-uofg <>
2021-10-13 19:30:01 +0100

    change to seconds mean_drift

--- mma/mma_gui.py
+++ mma/mma_gui.py
@@ -88,7 +88,7 @@ def main():
         right_column.write('')
 
         drift_mean = np.mean(df['RT drift (min)'])
-        st.write(drift_mean)
+        st.write('The mean RT drift is ', drift_mean, ' min.')
         if np.abs(drift_mean) < 0.5:
             st.write('The drift is quite minimal. Do you wish to continue with the anaysis?')
         else:
